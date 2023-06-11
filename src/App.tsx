993b41a233e926860e403700b9022a4540cee4a4
Bennett Smrdel <fireknights318@gmail.com>
2023-06-11 08:26:54 -0500

    name

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           <img src="/images/vite.svg" className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + Reacttttt</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
